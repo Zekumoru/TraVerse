6449227d0baddd9e49a0a9a598b85fa396e2d9a6
Zekumoru Dragonhart <57686690+Zekumoru@users.noreply.github.com>
2025-01-21 12:14:58 +0100

    Setup favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,23 @@ export const metadata: Metadata = {
   title: 'TraVerse',
   description:
     'A seamless communication chat web application between people who speak different languages.',
+  icons: {
+    icon: [
+      {
+        rel: 'icon',
+        url: '/favicon-96x96.png',
+        sizes: '96x96',
+        type: 'image/png',
+      },
+      { rel: 'icon', url: '/favicon.svg', type: 'image/svg+xml' },
+      { rel: 'shortcut icon', url: '/favicon.ico' },
+    ],
+    apple: '/apple-touch-icon.png',
+  },
+  manifest: '/site.webmanifest',
+  other: {
+    'apple-mobile-web-app-title': 'TraVerse',
+  },
 };
 
 export default function RootLayout({
