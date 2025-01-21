0bc5e64262844f2df48906c4f42f0c4329fd20a6
Zekumoru Dragonhart <57686690+Zekumoru@users.noreply.github.com>
2025-01-21 14:21:29 +0100

    Add color to catchphrase

--- app/pages/SignInPage.tsx
+++ app/pages/SignInPage.tsx
@@ -6,13 +6,17 @@ export default function SignInPage() {
   return (
     <div className="grid content-center min-h-screen p-4 gap-6 text-5xl text-center leading-[72px]">
       <Logo />
+
+      {/* Mobile */}
       <div>
         Conversations
         <br /> made universal.
       </div>
-      <div>
-        Just for <span>you</span>.
+      <div className="font-bold">
+        Just for <span className="text-primary-600">you</span>.
       </div>
+      {/* End Mobile */}
+
       <GoogleSignInButton />
 
       <div className="absolute bottom-0 left-0 right-0 text-base p-4">
