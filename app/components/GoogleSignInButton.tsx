60d9d5b38257f336269cba73222f108ff246d8f2
Zekumoru Dragonhart <57686690+Zekumoru@users.noreply.github.com>
2025-01-21 14:58:31 +0100

    Add hover/active color to Google sign-in button

--- app/components/GoogleSignInButton.tsx
+++ app/components/GoogleSignInButton.tsx
@@ -10,7 +10,7 @@ export default function SignInButton() {
         await signIn('google');
       }}
     >
-      <button className="p-4 rounded-full shadow-md text-2xl flex gap-4 w-full max-w-[575px] mx-auto">
+      <button className="p-4 rounded-full shadow-md text-2xl flex gap-4 w-full max-w-[575px] mx-auto hover:bg-neutral-100 active:bg-neutral-200 transition">
         <Image src="/assets/google-icon.svg" alt="" width={32} height={32} />
         <span className="flex-1">Sign in with Google</span>
       </button>
